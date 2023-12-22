dac012915b099a3ab049b9ac05f55b4874db5ec1
MenukaRanasinghe <88645663+MenukaRanasinghe@users.noreply.github.com>
2023-12-22 22:09:21 +0530

    Initial commit

---
+++ ShoppingApp_BackEnd/src/test/java/com/example/ShoppingApp_BackEnd/ShoppingAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoppingApp_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
